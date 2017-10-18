e088b7a3a246635255d2d58680947d01b86c41f2
Trymunx <trymunx@gmail.com>
2017-10-18 18:31:46 +0200

    Add default prose length, add word generator function

--- Generator.js
+++ Generator.js
@@ -1,8 +1,21 @@
 const Runes = require("./Runes.json");
 
 function generateText (numWords) {
+
+  if (!numWords) {
+    numWords = 200;
+  }
+
   for (let i = 0; i < numWords; i++) {
     // Generate words
   }
 }
 
+function generateWord (wordLength) {
+  for (let i = 0; i < wordLength; i++) {
+    // Concatenate runes from Runes.normal and Runes.pillared
+    // up to wordLength. Runes should be randomly chosen, but
+    // need some rules to place syllables together and not 
+    // just a load of pillared consonants / vowels together
+  }
+}
